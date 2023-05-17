afcfa9c73cf7bfccf2107e8c3a1deb0fce1f4e49
regadior <regadior@gmail.com>
2023-05-17 13:21:39 +0200

    fix: 🐛 Fixed roles are no longer duplicated
    ✅ Closes: #13

--- backend/src/user/infrastructure/initializer/user.rol.entity.initializer.ts
+++ backend/src/user/infrastructure/initializer/user.rol.entity.initializer.ts
@@ -1,23 +1,36 @@
 import { UserRolEntity } from 'src/user/infrastructure/entity/user.rol.entity';
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { UserRolRepository } from 'src/user/domain/repository/user.rol.repository';
 
 @Injectable()
 export class UserRolInitializer implements OnModuleInit {
+  constructor(
+    @Inject(UserRolRepository)
+    private readonly userRolRepository: UserRolRepository,
+  ) {}
+
   async onModuleInit() {
     await this.createDefaultUserRoles();
   }
   async createDefaultUserRoles() {
     //Initialize rol User
-    const userRolEntity1 = new UserRolEntity();
-    userRolEntity1.userRolId = 0;
-    userRolEntity1.name = 'user';
-    userRolEntity1.description = 'Normal User';
-    userRolEntity1.save();
+    const rol1 = await this.userRolRepository.findOneByName('user');
+    if (rol1 == null) {
+      const userRolEntity1 = new UserRolEntity();
+      userRolEntity1.userRolId = 0;
+      userRolEntity1.name = 'user';
+      userRolEntity1.description = 'Normal User';
+      userRolEntity1.save();
+    }
+
     //Initialize rol Admin
-    const userRolEntity2 = new UserRolEntity();
-    userRolEntity2.userRolId = 0;
-    userRolEntity2.name = 'admin';
-    userRolEntity2.description = 'User with all permissions';
-    userRolEntity2.save();
+    const rol2 = await this.userRolRepository.findOneByName('admin');
+    if (rol2 == null) {
+      const userRolEntity2 = new UserRolEntity();
+      userRolEntity2.userRolId = 0;
+      userRolEntity2.name = 'admin';
+      userRolEntity2.description = 'User with all permissions';
+      userRolEntity2.save();
+    }
   }
 }
