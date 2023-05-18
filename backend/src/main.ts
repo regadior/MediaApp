c926cebf243523406aa856879c79e336fdd249e4
regadior <regadior@gmail.com>
2023-05-18 16:15:02 +0200

    fix: 🐛 Config basic cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = process.env.PORT || '8000';
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
