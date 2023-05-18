0d5165ab1c191787c70dd1ddb38edeed8cd17889
regadior <regadior@gmail.com>
2023-05-18 16:27:18 +0200

    fix: 🐛 Fixed cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = process.env.PORT || '8000';
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(port);
 }
 bootstrap();
