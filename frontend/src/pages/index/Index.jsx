66aeeb9468841495acc637144c6c0cdfcec688a6
regadior <regadior@gmail.com>
2023-06-04 10:10:08 +0200

    feat: 🎸 Added Plattform filter

--- frontend/src/pages/index/Index.jsx
+++ frontend/src/pages/index/Index.jsx
@@ -2,18 +2,28 @@ import React, { useState } from "react";
 import "./Index.css";
 import GameCards from "../../components/game-cards/GameCards";
 import SearchGameBar from "../../components/search-game-bar/SearchGameBar";
+import PlattformFilter from "../../components/plattform-filter/PlattformFilter";
 export default function Inicio() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedPlatform, setSelectedPlatform] = useState("");
   const handleSearch = (term) => {
     setSearchTerm(term);
   };
+  const handlePlatformSelection = (platform) => {
+    setSelectedPlatform(platform);
+  };
+  
+
   return (
     <div className="index_total">
       <div className="index_filtros">
         <SearchGameBar onSearch={handleSearch} />
+        <PlattformFilter
+          onPlatformSelection={handlePlatformSelection}
+        />
       </div>
       <div className="index_cards">
-        <GameCards searchTerm={searchTerm} />
+        <GameCards searchTerm={searchTerm} selectedPlatform={selectedPlatform}  />
       </div>
     </div>
   );
